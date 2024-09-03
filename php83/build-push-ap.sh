1e566e7e852c48d645614fb96555df11591e3fac
Herdi Yudha Yusuf <mangkibo.boo@gmail.com>
2024-09-03 14:43:04 +0700

    update certificate 8

--- php83/build-push-ap.sh
+++ php83/build-push-ap.sh
@@ -1,3 +1,3 @@
-docker build -t php:8.3-apache-ubuntu -f Dockerfile.php8-apache-deb .
+docker build -t php:8.3-apache-ubuntu -f Dockerfile.php8apache-deb .
 docker image tag php:8.3-apache-ubuntu kiboaleph/php:8.3-apache-ubuntu
 docker push kiboaleph/php:8.3-apache-ubuntu
