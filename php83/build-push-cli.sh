363cd89a13ce322c34af2b95e945d518ec059cfd
Herdi Yudha Yusuf <mangkibo.boo@gmail.com>
2024-09-04 10:22:30 +0700

    fix cli

--- php83/build-push-cli.sh
+++ php83/build-push-cli.sh
@@ -1,3 +1,3 @@
-docker build -t php:8.3cli-alpine -f Dockerfile.php8cli-alp .
-docker image tag php:8.3cli-alpine kiboaleph/php:8.3cli-alpine
-docker push kiboaleph/php:8.3cli-alpine
+docker build -t php:8.3-cli-alpine -f Dockerfile.php8cli-alp .
+docker image tag php:8.3-cli-alpine kiboaleph/php:8.3-cli-alpine
+docker push kiboaleph/php:8.3-cli-alpine
