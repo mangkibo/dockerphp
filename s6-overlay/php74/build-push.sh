4747e16fd49bdc2905b31db0512464c6412622b9
Herdi Yudha Yusuf <mangkibo.boo@gmail.com>
2024-09-06 21:06:05 +0700

    build push command

--- s6-overlay/php74/build-push.sh
+++ s6-overlay/php74/build-push.sh
@@ -1,3 +1,3 @@
-docker build -t php:7.4-serversideup -f Dockerfile
+docker build -t php:7.4-serversideup -f Dockerfile .
 docker image tag php:7.4-serversideup kiboaleph/php:7.4-serversideup
 docker push kiboaleph/php:7.4-serversideup
--- s6-overlay/php83/build-push.sh
+++ s6-overlay/php83/build-push.sh
@@ -1,3 +1,3 @@
-docker build -t php:8.3-serversideup -f Dockerfile
+docker build -t php:8.3-serversideup -f Dockerfile .
 docker image tag php:8.3-serversideup kiboaleph/php:8.3-serversideup
 docker push kiboaleph/php:8.3-serversideup
