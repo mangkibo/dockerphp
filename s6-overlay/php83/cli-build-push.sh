aa3d446a6a3d3a965d87908e1256bf06ccb7f308
Herdi Yudha Yusuf <mangkibo.boo@gmail.com>
2024-09-10 03:36:04 +0700

    add nginx serversideup alpine

--- s6-overlay/php83/cli-build-push.sh
+++ s6-overlay/php83/cli-build-push.sh
@@ -1,3 +1,3 @@
-docker build -t php:8.3-serversideup-cli -f Dockerfile.cli-alp .
-#docker image tag php:8.3-serversideup-cli kiboaleph/php:8.3-serversideup-cli
-#docker push kiboaleph/php:8.3-serversideup-cli
+docker build -t php:8.3-serversideup-alp-cli -f Dockerfile.cli-alp .
+docker image tag php:8.3-serversideup-alp-cli kiboaleph/php:8.3-serversideup-alp-cli
+docker push kiboaleph/php:8.3-serversideup-alp-cli
