9c43e9c68d217ea784e1eefdd81570c4b3c77753
Herdi Yudha Yusuf <mangkibo.boo@gmail.com>
2024-09-10 03:16:04 +0700

    add cli serverside alpine

---
+++ s6-overlay/php83/cli-build-push.sh
@@ -0,0 +1,3 @@
+docker build -t php:8.3-serversideup-cli -f Dockerfile.cli-alp .
+#docker image tag php:8.3-serversideup-cli kiboaleph/php:8.3-serversideup-cli
+#docker push kiboaleph/php:8.3-serversideup-cli
