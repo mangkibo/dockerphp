1b22a5891ff9b7798210f3c7696c91be3e3519ee
Herdi Yudha Yusuf <mangkibo.boo@gmail.com>
2024-09-12 03:16:00 +0700

    add new install odbc

--- s6-overlay/php83/build-push.sh
+++ s6-overlay/php83/build-push.sh
@@ -1,3 +1,3 @@
 docker build -t kibophp:8.3-serversideup -f Dockerfile .
 docker image tag kibophp:8.3-serversideup kiboaleph/php:8.3-serversideup
-#docker push kiboaleph/php:8.3-serversideup
+docker push kiboaleph/php:8.3-serversideup
