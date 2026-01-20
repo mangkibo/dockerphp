3a82e96be7d5bd763816365a25e63e6c991d62e7
mangkibo <herdi@aleph-labs.com>
2026-01-20 16:49:59 +0700

    php 84

--- s6-overlay/php84/alp-build-push.sh
+++ s6-overlay/php84/alp-build-push.sh
@@ -1,3 +1,3 @@
-docker build -t php:8.3-serversideup-alp-nginx -f Dockerfile.alp .
-docker image tag php:8.3-serversideup-alp-nginx kiboaleph/php:8.3-serversideup-alp-nginx
-docker push kiboaleph/php:8.3-serversideup-alp-nginx
+docker build -t php:8.4-serversideup-alp-nginx -f Dockerfile.alp .
+docker image tag php:8.4-serversideup-alp-nginx kiboaleph/php:8.4-serversideup-alp-nginx
+docker push kiboaleph/php:8.4-serversideup-alp-nginx
